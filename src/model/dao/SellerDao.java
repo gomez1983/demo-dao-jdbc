8587c0a688a95962e54393eb735d53ff97c5db32
Andre Gomez <andregc1983@gmail.com>
2020-08-23 11:30:44 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -11,5 +12,9 @@ public interface SellerDao {
 	void deleteById(Seller obj); // Deleta no banco de dados o "obj" que enviar como parâmetro de entrada
 	Seller findById(Integer id); // Pega o Id e consulta no banco de dados o objeto com essa "id". Se existir, retorna. Se não, retorna nulo.
 	List<Seller> findAll(); // "findAll" retorna todos os vendedores.
+	
+	List<Seller> findByDepartment(Department department); // Operação para buscar por departamento
+	
+	
 	
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -89,6 +92,50 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? " // *getId definira o código desse departamento 
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId()); // * Esse getId define o código do departamento
+			rs = st.executeQuery(); // Esse comando acima é executado e o resultado cai em result set "rs"
+			
+			List<Seller> list = new ArrayList<>();
+			
+//---------Um Map vazio é criado para guardar qualquer departamento que for instanciado.		
+			Map<Integer, Department> map = new HashMap<>(); // Integer é o id do departamento. Map é usado aqui para não repetir a instanciação do departamento. Só existe 1 classe departamento, e é dela que cada objeto vendedor é instanciada.
+			
+			while (rs.next()) { // Como pode vir um resultado nulo (nenhum departamento), temos que usar o while no lugar do if. Isso testa se veio algum resultado. Caso não retorne registro, o rs conect dá falto e pula o "if". O vendedor, no caso, é nulo (não tem departamento com o id).
+				
+				Department dep = map.get(rs.getInt("DepartmentID")); // Testa se o departamento já existe. Caso sim, o map.get pega esse departamento
+				
+				if (dep == null) { // Se der nulo, significa que o departamento não existia. Neste caso, aí sim o departamento é instanciado.
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); // Departamento instanciado porque o if deu nulo.
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj); // Quando esgotar o result set, todos serão adicionados a lista. 
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 }
